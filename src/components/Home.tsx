967bd2804ca7fd371a64cf164b6b86c457e4f1fc
David <dbonnaud123@gmail.com>
2022-12-15 22:42:05 -0500

    added transitions on component display change

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,11 @@
+import { Box, Fade } from '@mui/material'
 import React from 'react'
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <Box sx={{ display: 'flex' }}>
+      <Fade in timeout={1000}><h1>Home</h1></Fade>
+    </Box>
   )
 }
 
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,8 +1,11 @@
+import { Box, Fade } from '@mui/material'
 import React from 'react'
 
 const Portfolio = () => {
   return (
-    <h1>Portfolio</h1>
+    <Box sx={{ display: 'flex' }}>
+      <Fade in timeout={1000}><h1>Portfolio</h1></Fade>
+    </Box>
   )
 }
 
