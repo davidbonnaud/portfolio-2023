3bf2c8591246c6b5ed6d22c77c81c76180e072f2
David Bonnaud <dbonnaud123@gmail.com>
2022-12-20 18:42:13 -0800

    cleaned up css, added repsonsive about blurb, need to add social stuff below it after the typing effect finishes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,20 +4,23 @@ import Typewriter from 'typewriter-effect';
 
 const About = () => {
   return (
-    <Box sx={{ display: 'flex', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
-      <Fade in timeout={1000}>
-        <h1>About</h1>
-      </Fade>
-      <div className='intro'>
+    <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+      <Box className='intro' sx={{ textAlign: 'center' }}>
+        <Fade in timeout={1000}>
+          <h1>About</h1>
+        </Fade>
+      </Box>   
+
+      <Box className='intro' sx={{ textAlign: 'center', maxWidth: '75%' }}>
         <Typewriter 
           onInit={(typewriter) => {
-            typewriter.typeString("Hi, I'm David Bonnaud. <br/>").pauseFor(1000).typeString("I'm a web developer with experience in full stack development using Python/Django, JavaScript, Ruby on Rails, React, and TypeScript. <br/>").pauseFor(1000).typeString("I graduated from CSUF with a Bachelor's of Science in Computer Science.").start();
+            typewriter.typeString("Hi, I'm David Bonnaud.<br/>").pauseFor(1000).typeString("I'm a developer currently based out of Cambridge, MA with a certification in full stack web development from Southern Methodist University.").pauseFor(1000).typeString(" I graduated from California State University, Fullerton with a Bachelor's of Science in Computer Science.").pauseFor(1000).typeString(" Some of the languages and frameworks I have experience with are JavaScript, Python, Django, React, TypeScript, and Ruby on Rails.").start();
           }}
           options={{
-            delay: 40
+            delay: 30
           }}
         />
-      </div>
+      </Box>        
     </Box>
   )
 }
