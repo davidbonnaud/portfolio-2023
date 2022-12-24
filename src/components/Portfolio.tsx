e62805647c872953fcafaf63257f36b8e360c1c5
David Bonnaud <dbonnaud123@gmail.com>
2022-12-24 05:06:44 -0800

    made some minor spacing fixes to the portfolio cards, need to adjust picture sizing to not stretch or cutoff as much or at least center it and add pagination to the project items as well as pictures

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -3,7 +3,7 @@ import Grid2 from '@mui/material/Unstable_Grid2';
 import React from 'react';
 
 const Portfolio = () => {
-  const projects = ['FishHooked', 'Flixter', 'Nomster', 'Grammable', 'Budgety', 'Flashback'];
+  const projects = [['FishHooked', 'E-commerce platform built using Next.js and Sanity as a headless CMS. Handles transactions with Stripe API.'], ['Flixter', 'A two-sided, video-streaming marketplace platform that features credit card payment capabilities, user role management, complex user interfaces, and advanced database relationships.'], ['Nomster', 'A Yelp clone that integrates with the Google Maps API and includes features like user comments, star ratings, image uploading, and user authentication.'], ['Grammable', 'An Instagram clone that was built using industry-standard, test-driven development following numerous red/green/refactor cycles.'], ['Budgety', 'A budgeting web application built using vanilla JavaScript, HTML, and CSS.'], ['Flashback', 'A React flashcard app.']];
 
   return (
     <Box sx={{ maxWidth: '100%', maxHeight: '100%' }}>
@@ -18,25 +18,25 @@ const Portfolio = () => {
           return (
             <Fade in timeout={1200}>
               <Grid2 sx={{ zIndex: 1 }} xs={3} >
-                <Card sx={{ maxWidth: 400, maxHeight: 400 }}>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      height="200"
-                      image={require('./portfolio-images/budgety.png')}
-                      alt={project}
-                    />
-                    <Divider sx={{ borderBottomWidth: 2, borderColor: 'light-gray' }} />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        {project}
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        Budgeting app built with JavaScript, HTML, and CSS. 
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
+              <CardActionArea>
+                <Card sx={{ width: '100%', height: 350, justifyContent: 'center' }}>
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image={require('./portfolio-images/budgety.png')}
+                    alt={project[0]}
+                  />
+                  <Divider sx={{ borderBottomWidth: 2, borderColor: 'light-gray' }} />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {project[0]}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {project[1]} 
+                    </Typography>
+                  </CardContent>
                 </Card>
+                </CardActionArea>
               </Grid2>
             </Fade>
           )
