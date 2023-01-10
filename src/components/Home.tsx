448009038d70bb672fe321685ba92d8f3a3e6692
David Bonnaud <dbonnaud123@gmail.com>
2023-01-10 03:38:46 -0800

    added parallax tilt effect and boilerplate, need to find and add assets as well as space for design and mobile

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,17 @@
 import { Box, Fade } from '@mui/material';
 import React from 'react';
+import Tilt from 'react-parallax-tilt';
 
 
 const Home = () => {
   return (
     <Box sx={{ display: 'flex', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
       <Fade in timeout={1000}>
-        {/* FADE IN NAME, WAIT A SEC THEN TYPEWRITER THE BIO/INTRO TEXT */}
-      <h1>Home</h1>
-        
+        <h1>Home</h1>        
       </Fade>
+      {/* <Tilt className="parallax-effect" perspective={500}>
+        <img className='inner-element' src={require('./portfolio-images/budgety.png')} alt='budgetyimg' />
+      </Tilt> */}
     </Box>
   )
 }
