8e0671cdf727f8f293e45caf43792795015cdcff
David Bonnaud <dbonnaud123@gmail.com>
2023-01-19 23:14:12 -0800

    working on optimization

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -2,6 +2,7 @@ import { Box, Card, CardActionArea, CardContent, CardMedia, Divider, Fade, Pagin
 import Grid2 from '@mui/material/Unstable_Grid2';
 import React, { useState } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import projects from './portfolio-data.json';
 
 declare module '@mui/material/styles' {
   interface Theme {
@@ -34,7 +35,7 @@ declare module '@mui/material/styles' {
 }
 
 const Portfolio = () => {
-  const projects = [['Current Portfolio Site', 'A portfolio website I designed and created using Typescript-React, Material UI, and tsparticles. Designed with mobile platforms in mind and responsive UI for most other platforms as well. Currently in development with additional functionalities planned.', 'portfolio-2023.png', 'https://github.com/davidbonnaud/portfolio-2023'], ['FishHooked', 'An E-commerce platform built using Next.js and Sanity as a headless CMS. Handles transactions with Stripe API.', 'fishhooked.png', 'https://github.com/davidbonnaud/em-shop'], ['Flixter', 'A two-sided, video-streaming marketplace platform that features credit card payment capabilities, user role management, complex user interfaces, and advanced database relationships.', 'flixter.jpg', 'https://github.com/davidbonnaud/flixter'], ['Nomster', 'A Yelp clone that integrates with the Google Maps API and includes features like user comments, star ratings, image uploading, and user authentication.', 'nomster.jpg', 'https://github.com/davidbonnaud/nomster'], ['Grammable', 'An Instagram clone that was built using industry-standard, test-driven development following numerous red/green/refactor cycles.', 'grammable.png', 'https://github.com/davidbonnaud/grammable'], ['2020 Portfolio Site', 'A portfolio site I made following a Udemy course, written in HTML/CSS and Vanilla JavaScript.', 'portfolio-2020.png', 'https://github.com/davidbonnaud/my-portfolio-site'], ['Budgety', 'A budgeting web application built using vanilla JavaScript, HTML, and CSS.', 'budgety.png', 'https://github.com/davidbonnaud/budgety'], ['Flashback', 'A React flashcard app using states to manage card positions and flipping functionality.', 'flashback.png', 'https://github.com/davidbonnaud/flashback']];
+  // const projects = [['Current Portfolio Site', 'A portfolio website I designed and created using Typescript-React, Material UI, and tsparticles. Designed with mobile platforms in mind and responsive UI for most other platforms as well. Currently in development with additional functionalities planned.', 'portfolio-2023.png', 'https://github.com/davidbonnaud/portfolio-2023'], ['FishHooked', 'An E-commerce platform built using Next.js and Sanity as a headless CMS. Handles transactions with Stripe API.', 'fishhooked.png', 'https://github.com/davidbonnaud/em-shop'], ['Flixter', 'A two-sided, video-streaming marketplace platform that features credit card payment capabilities, user role management, complex user interfaces, and advanced database relationships.', 'flixter.jpg', 'https://github.com/davidbonnaud/flixter'], ['Nomster', 'A Yelp clone that integrates with the Google Maps API and includes features like user comments, star ratings, image uploading, and user authentication.', 'nomster.jpg', 'https://github.com/davidbonnaud/nomster'], ['Grammable', 'An Instagram clone that was built using industry-standard, test-driven development following numerous red/green/refactor cycles.', 'grammable.png', 'https://github.com/davidbonnaud/grammable'], ['2020 Portfolio Site', 'A portfolio site I made following a Udemy course, written in HTML/CSS and Vanilla JavaScript.', 'portfolio-2020.png', 'https://github.com/davidbonnaud/my-portfolio-site'], ['Budgety', 'A budgeting web application built using vanilla JavaScript, HTML, and CSS.', 'budgety.png', 'https://github.com/davidbonnaud/budgety'], ['Flashback', 'A React flashcard app using states to manage card positions and flipping functionality.', 'flashback.png', 'https://github.com/davidbonnaud/flashback']];
 
   const totalPages = Math.ceil(projects.length / 4);
   const [currentPage, setCurrentPage] = useState(1);
@@ -75,22 +76,22 @@ const Portfolio = () => {
           return (
             <Fade in timeout={1000}>
               <Grid2 sx={{ zIndex: 1 }} xs={12} sm={6} lg={3} style={{ maxWidth: 500 }} >
-                <CardActionArea href={project[3]} target={'_blank'}>
+                <CardActionArea href={project.url} target={'_blank'}>
                   <Card sx={{ width: '100%', height: 350, justifyContent: 'center', backgroundColor: '#EFEFEF' }}>
                     <CardMedia
                       component="img"
                       height="200"
-                      image={require(`./portfolio-images/${project[2]}`)}
-                      alt={project[0]}
+                      image={require(`./portfolio-images/${project.img}`)}
+                      alt={project.name}
                       sx={{ objectFit: "contain" }}
                     />
                     <Divider sx={{ borderBottomWidth: 2, borderColor: 'light-gray' }} />
                     <CardContent>
                       <Typography gutterBottom variant="h5" component="div">
-                        {project[0]}
+                        {project.name}
                       </Typography>
                       <Typography variant="body2" color="text.secondary" sx={{ fontSize: {xs: 12, md: 14} }}>
-                        {project[1]} 
+                        {project.desc} 
                       </Typography>
                     </CardContent>
                   </Card>
